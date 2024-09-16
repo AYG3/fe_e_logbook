28e6ba25199025ffd8506c7e69e8d7cdf87e7650
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-16 11:57:54 +0100

    Refactor CreateEntry.jsx to remove unused imports and simplify handleCreateEntry function

--- src/pages/CreateEntry.jsx
+++ src/pages/CreateEntry.jsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton.jsx';
 import { handleCreateEntry } from '../api/user.js';
