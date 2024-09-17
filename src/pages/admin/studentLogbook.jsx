e809013144cd4783f133ed8f9eec4da97f643f5c
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-17 11:56:01 +0100

    Refactor studentLLogbook component to studentLogbook component

---
+++ src/pages/admin/studentLogbook.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+const studentLogbook = () => {
+
+    useEffect(() => {
+
+    })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default studentLogbook
