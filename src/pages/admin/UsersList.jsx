a57478465841cfb4a4a2f58721e679cf97f04a02
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-17 11:31:06 +0100

    Refactor UsersList component to correctly fetch and display users from the server

--- src/pages/admin/UsersList.jsx
+++ src/pages/admin/UsersList.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import axiosInstance from '../../utils/axiosConfig'
-
+import React, { useEffect, useState } from 'react';
+import axiosInstance from '../../utils/axiosConfig';
+import { formatDate } from '../../utils/dateTimeUtils';
+import { Link } from 'react-router-dom';
 const UsersList = () => {
-
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -11,26 +11,42 @@ const UsersList = () => {
         const res = await axiosInstance.get(`/logbook/admin/users`);
         console.log(res.data);
 
-        setUsers(res.data)
-
+        setUsers(res.data);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
 
     getUsers();
-  }, [])
+  }, []);
 
   return (
-    <div>
-      <div>
-        USERS
-      </div>
-      {users?.map((student, index) => {
-        <p key={student._id}>{student.fname}</p>
-      })}
+    <div className="container mx-auto p-4">
+      <h2 className="text-2xl font-bold mb-4">Users</h2>
+      <table className="min-w-full bg-white">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 border-b">ID</th>
+            <th className="py-2 px-4 border-b">First Name</th>
+            <th className="py-2 px-4 border-b">Last Name</th>
+            <th className="py-2 px-4 border-b">Started</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((student) => (
+            <tr key={student._id}>
+              <Link to={`/logbooks/${student._id}`}>
+              <td className="py-2 px-4 border-b">{student._id}</td>
+              <td className="py-2 px-4 border-b">{student.fname}</td>
+              <td className="py-2 px-4 border-b">{student.lname}</td>
+              <td className="py-2 px-4 border-b">{formatDate(student.createdAt)}</td>
+              </Link>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default UsersList
+export default UsersList;
