99ca2d573ddc03e59771c62dc5cdb4797c119ff3
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-17 09:06:53 +0100

    Refactor UsersList component to fetch and display users from the server

--- src/pages/admin/UsersList.jsx
+++ src/pages/admin/UsersList.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../utils/axiosConfig'
 
 const UsersList = () => {
+
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await axiosInstance.get(`auth/admin/users`);
+        console.log(res);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getUsers();
+  }, [])
   return (
     <div>
       Users List
