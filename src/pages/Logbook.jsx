00b832461ec2ff2a66d1d5664ae1287b33087ff5
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-25 20:53:25 +0100

    Refactor Logbook component to improve table layout and add missing line breaks

--- src/pages/Logbook.jsx
+++ src/pages/Logbook.jsx
@@ -90,12 +90,12 @@ const Logbook = () => {
           </Link>
         </div>
 
-        <table className="w-full border border-separate border-spacing-2 table-auto ">
+        <table className="w-full border border-separate border-spacing-2 table-auto">
           <thead>
             <tr>
               <th className="border border-slate-600 rounded-md">No</th>
               <th className="border border-slate-600 rounded-md">Day</th>
-              <th className="border border-slate-600 rounded-md">
+              <th className="border border-slate-600 rounded-md w-max">
                 Nature of Activities
               </th>
               <th className="border border-slate-600 rounded-md">Operations</th>
@@ -103,54 +103,48 @@ const Logbook = () => {
           </thead>
           <tbody>
             {entries?.map((entry, index) => {
-              if (index > 0 && index % 5 == 0){
-                week_id++
+              if (index > 0 && index % 5 == 0) {
+                week_id++;
               }
-              if(!weeks.includes(week_id)) {
+              if (!weeks.includes(week_id)) {
                 weeks.push(week_id);
               }
               return (
-              <React.Fragment key={entry._id}>
-                {index> 0 && index % 5 == 0 && (<tr><td colSpan={4} className="border h-16 bg-slate-800 text-center rounded-md text-white font-bold"></td></tr>)}
+                <React.Fragment key={entry._id}>
+                  {index > 0 && index % 5 == 0 && (
+                    <tr>
+                      <td colSpan={4} className="border h-16 bg-slate-800 text-center rounded-md text-white font-bold"></td>
+                    </tr>
+                  )}
                   <tr id={week_id}>
                     {/* <tr>Week {week_id}</tr> */}
                     <td className="border border-slate-700 rounded-md text-center">
-                      {index % 5 + 1} 
+                      {index % 5 + 1}
                     </td>
                     <td className="border border-slate-700 rounded-md text-center">
-                      {entry.day}<br/>
+                      {entry.day}
+                      <br />
                       {entry.date}
                     </td>
-                    <td className="border border-slate-700 rounded-md whitespace-pre-wrap pl-4">
+                    <td className="border border-slate-700 rounded-md whitespace-pre-wrap max-w-fit pl-4">
                       {entry.nature_of_activities}
                     </td>
-  
                     <td className="border border-slate-700 rounded-md text-center">
                       <div className="flex justify-center gap-4">
-                        <Link
-                          to={`/details/${entry._id}`}
-                          className="text-2x1 text-green-800"
-                        >
+                        <Link to={`/details/${entry._id}`} className="text-2x1 text-green-800">
                           <BsInfoCircle />
                         </Link>
-                        <Link
-                          to={`/edit/${entry._id}`}
-                          className="text-2x1 text-yellow-600"
-                        >
+                        <Link to={`/edit/${entry._id}`} className="text-2x1 text-yellow-600">
                           <AiOutlineEdit />
                         </Link>
-                        <Link
-                          to={`/delete/${entry._id}`}
-                          className="text-2x1 text-red-600"
-                        >
+                        <Link to={`/delete/${entry._id}`} className="text-2x1 text-red-600">
                           <MdOutlineDelete />
                         </Link>
                       </div>
                     </td>
                   </tr>
-              </React.Fragment>
+                </React.Fragment>
               );
-              
             })}
           </tbody>
         </table>
