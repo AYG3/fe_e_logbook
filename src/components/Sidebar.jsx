2f9981580639b4ffb1ab1b0991b6bcb1a67111f4
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-30 09:46:23 +0100

    Refactor Sidebar component to add navigation links

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Logbook</li>
+            <li></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar
