95c29a37845d1e21b898813186529d73823acfd8
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-10-02 15:35:09 +0100

    Refactor DetailsEntry component to replace delete link with modal and import DeleteModal component

--- src/pages/DeleteEntry.jsx
+++ src/pages/DeleteEntry.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import BackButton from "../components/BackButton";
 import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../utils/axiosConfig";
+import DeleteModal from "../components/Modals/DeleteModal";
 
 const DeleteEntry = () => {
   const { id } = useParams();
@@ -34,27 +35,7 @@ const DeleteEntry = () => {
         </button>
       </div>
 
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg w-[400px]">
-            <h2 className="text-2xl mb-4">Confirm Deletion</h2>
-            <p className="mb-4">Are you sure you want to delete this entry?</p>
-            <div className="flex justify-end gap-4">
-              <button
-                className="p-2 bg-gray-300 rounded"
-                onClick={() => setIsModalOpen(false)}
-              >
-                Cancel
-              </button>
-              <button
-                className="p-2 bg-red-600 text-white rounded"
-                onClick={handleDeleteEntry}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+      {isModalOpen && ( <DeleteModal   />
       )}
     </div>
   );
