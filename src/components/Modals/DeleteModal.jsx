ae5e577597fab3dc08ef8cef75adb4cdd8055bc5
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-10-04 18:46:22 +0100

    Refactor DeleteModal component to include id parameter from URL

--- src/components/Modals/DeleteModal.jsx
+++ src/components/Modals/DeleteModal.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosConfig';
 
 const DeleteModal = () => {
+  
   const { id } = useParams();
   const navigate = useNavigate();
 
