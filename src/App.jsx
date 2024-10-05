59f5c249db638b489c3b452816c927b7f0fe7a2f
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-10-05 10:30:49 +0100

    Refactor App component to remove commented out code for Navbar route

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,6 @@ import Navbar from './components/shared/navbar';
 import ConfirmUserDelete from './pages/admin/ConfirmUserDelete';
 import AdminHome from './pages/admin/AdminHome';
 import UserProfile from './pages/UserProfile';
-import UserAuthContext from './context/user/UserAuthContext';
-import AdminAuthContext from './context/admin/AdminAuthContext';
-import AdminNavbar from './components/shared/AdminNavbar';
-import UserNavbar from './components/shared/UserNavbar';
 
 const App = () => {
   
@@ -31,7 +27,6 @@ const App = () => {
     <Layout>
       <Routes>
         <Route path='/' element={<Home/>} />
-        {/* <Route path='/navbar' element={<Navbar/>} /> */}
         <Route path='/login' element={<Login />}/>
         <Route path='/signup' element={<SignUp />}/>
         <Route path='/logbooks' element={<Logbook />}/>
