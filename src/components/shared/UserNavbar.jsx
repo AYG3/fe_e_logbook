68aa01e9c19ab900ccc4630e501d9ec33b96a74a
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-10-05 11:14:52 +0100

    Refactor AdminNavbar and UserNavbar components to remove unused code and update form layout

--- src/components/shared/UserNavbar.jsx
+++ src/components/shared/UserNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { UserAuthContext } from "../../context/user/UserAuthContext";
 
@@ -7,36 +7,7 @@ const UserNavbar = () => {
   const current_url = location.pathname;
 
   const navigate = useNavigate();
-  const { isLoggedIn, userLogout, userName, userId } = useContext(UserAuthContext);
-
-  const [weeks, setWeeks] = useState([]);
-  const [dropDown, setDropDown] = useState(false);
-
-  useEffect(() => {
-    const logbookWeeks = JSON.parse(localStorage.getItem("weeks"));
-    setWeeks(logbookWeeks || []);
-  }, []);
-
-  const toggleDropdown = () => {
-    setDropDown(!dropDown);
-  };
-
-  const handleScroll = (id) => {
-    if (current_url !== `/logbooks`) {
-      navigate("/logbooks");
-    }
-
-    const week = document.getElementById(id);
-    if (week) {
-      week.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  const handleLoginRedirect = () => {
-    if (current_url !== '/login') {
-      navigate('/login');
-    }
-  };
+  const { userLogout, userName, userId } = useContext(UserAuthContext);
 
   return (
     <nav className="bg-gray-800 p-4 shadow-md">
@@ -44,48 +15,13 @@ const UserNavbar = () => {
         <div className="text-white text-lg font-bold">
           <Link to="/">E-Logbook</Link>
         </div>
-        <div className="flex space-x-4">
-          <div className="relative">
-            <button
-              className="text-gray-300 hover:text-white px-4 py-2 rounded-md bg-gray-700 hover:bg-gray-600 border border-gray-600"
-              onClick={toggleDropdown}
-            >
-              Weeks
-            </button>
-            {dropDown && (
-              <div className="absolute bg-gray-800 text-white mt-2 rounded-xl shadow-lg w-48 z-10">
-                {weeks?.map((wk, index) => (
-                  <button
-                    key={index}
-                    className="block px-4 py-2 text-left hover:bg-gray-700 w-full"
-                    onClick={() => {
-                      handleScroll(wk);
-                      toggleDropdown();
-                    }}
-                  >
-                    Week {wk}
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
         <div className="flex items-center space-x-4">
-          {isLoggedIn ? (
-            <button
-              onClick={userLogout}
-              className="bg-slate-500 px-4 py-2 rounded text-white hover:bg-slate-600"
-            >
-              Logout
-            </button>
-          ) : (
-            <button
-              onClick={handleLoginRedirect}
-              className="bg-slate-500 px-4 py-2 rounded text-white hover:bg-slate-600"
-            >
-              Login
-            </button>
-          )}
+          <button
+            onClick={userLogout}
+            className="bg-slate-500 px-4 py-2 rounded text-white hover:bg-slate-600"
+          >
+            Logout
+          </button>
           <Link to={`/userprofile/${userId}`} className="text-white text-lg font-semibold">
             {userName}
           </Link>
