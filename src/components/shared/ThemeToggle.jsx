fb124cbe5b17a712151136dde79b75d5ed9f2e03
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-10-07 12:40:20 +0100

    Refactor ThemeToggle component to add dark mode functionality

--- src/components/shared/ThemeToggle.jsx
+++ src/components/shared/ThemeToggle.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ThemeToggle = () => {
+    const [theme, setTheme] = useState('light' || localStorage.getItem('theme'))
+
+    useEffect(() => {
+        if (theme === 'dark'){
+            document.documentElement.classList.add('dark')
+        }else{
+            document.documentElement.classList.remove('dark')
+        }
+        localStorage.setItem('theme', theme);
+    }, [theme])
+
   return (
     <div>
       
